378771883f5755e780116ad386a3a2a25a42a362
Pranto Mollick <prantocse01@gmail.com>
2024-11-01 13:00:02 +0600

    array and array list into

---
+++ java/5-array-arraylist/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        //Q: store a roll number;
+        int a = 19;
+
+        //Q: store a person's name
+        String name = "pranto Mollick";
+
+        // Q: store 5 roll numbers;
+
+        //syntax
+        //datatype[] variable_name = new datatype[size];
+        //store 5 roll numbers;
+//        int[] rollNumbers = new int[5];
+
+        //or directly all type of data array should be the same.
+//        int[] rollNumbers2 = {23, 12, 45, 32, 15};
+
+        int[] ros; //declaration of array. ros is getting defined in the stack;
+        ros = new int[5]; //initialisation: actually here object is being created in the memory (heap) at the runtime memory will allocate this is called run time allocation
+
+
+
+
+
+
+
+    }
+}
